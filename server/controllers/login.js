c4d14e053e14b1228516543f904c65c72443550e
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2024-06-03 21:11:55 +0530

    update

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -48,8 +48,10 @@ exports.userLogin = async (req, res) => {
       //name data options
 
       const options={
-    expiresIn:new Date (Date.now()+3*24*60*60*1000),
+    expires:new Date (Date.now()+3*24*60*60*1000),
     httpOnly:true,  //client side access nahi hoga
+    sameSit:'None',
+    secure:true,
       }
       res.cookie("token",token,options).status(200).json({
         succes: true,
