6903c4e809f0a66554bb258bf30d708783fa4305
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2024-06-03 17:15:54 +0530

    View Canteen Feature backend

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express=require("express");
 const app=express();
-
+const cookieParser=require("cookie-parser"); //for cookie parsing used for getting JWT Token
 require("dotenv").config();
 
 const PORT=process.env.PORT||3000;
 
-app.use(express.json());
+app.use(express.json()); 
 var cors = require("cors");
 app.use(cors());
-
+app.use(cookieParser());
 //fileUpload
 const fileupload = require("express-fileupload"); //server tak file upload kar paye
 app.use(
