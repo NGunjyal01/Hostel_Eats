6d0e334ca2a96ef77e0a5eb1ab3fe96daae4c6f9
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2024-06-11 15:39:58 +0530

    Update Profile Backend Done

--- server/controllers/profileSettings.js
+++ server/controllers/profileSettings.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
 const User=require("../models/user");
+const Merchant=require("../models/merchant");
+const Item=require("../models/item");
 const {isFileTypeSupported,uploadFileToCloudinary,} = require("../utils/cloudinary");
 require("dotenv").config();
 const jwt=require("jsonwebtoken");
@@ -77,3 +79,89 @@ await User.findByIdAndUpdate(
 }
 
 //Update Profile API
+exports.updateProfile = async(req,res) =>{
+  try{
+    const { token } = req.cookies;
+       if (!token) {
+         return res.status(200).json({
+           success: false,
+           message: "Your token is expired kindly login first",
+         });
+       }
+    const payload = await jwt.verify(token, process.env.JWT_SECRET);
+ 
+    const { firstName, lastName,phone, dob, gender } = req.body;
+
+    const exisitingUser = await User.findOne({ email:payload.email});
+    if (!exisitingUser) {
+      return res.status(200).json({
+        success: false,
+        message: "Not Registered please signup first",
+      });
+    }
+    const updateData = {};
+    const [day, month, year] = dob.split("-").map(Number);
+    const parsedDate = new Date(year, month - 1, day + 1);
+
+    if (!isNaN(parsedDate.getTime())) {
+      updateData.dob = parsedDate;
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid date format",
+      });
+    }
+   
+    if (gender) updateData.gender = gender;
+    if (dob) updateData.dob = parsedDate;
+    if(lastName)updateData.lastName=lastName;
+    if(firstName)updateData.firstName=firstName;
+    if(phone){
+    const existingPhone=await User.findOne({phone, _id : {$ne:exisitingUser._id}})
+    //console.log(existingPhone);
+    if(existingPhone){
+      return res.status(200).json({
+        success:false,
+        message:`Phone Number: ${phone} already exist in the database`,
+      })
+    }
+    updateData.phone = phone;
+  }
+    const updatedUser = await User.findByIdAndUpdate(payload.id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+    //Only accounts for which role is Owner
+    if(payload.role=="Owner"){
+      const existingCanteens=await Merchant.find({ownerEmail:payload.email});
+   
+      for (const canteen of existingCanteens) {
+        const canteenUpdateData = {};
+        if (firstName || lastName)
+          canteenUpdateData.ownerName = `${
+            firstName || canteen.ownerName.split(" ")[0]
+          } ${lastName || canteen.ownerName.split(" ")[1]}`;
+        if (phone) canteenUpdateData.ownerContact = phone;
+
+        await Merchant.findByIdAndUpdate(canteen._id, canteenUpdateData, {
+          new: true,
+          runValidators: true,
+        });
+      }
+    }
+  
+
+    res.status(200).json({
+      data:updatedUser,
+      success:true,
+      message:"Profile Updated successfully",
+    })
+  }
+  catch(error){
+    console.log(error);
+    return res.status(400).json({
+      success:false,
+      message:"Something Went Wrong",
+    })
+  }
+}
