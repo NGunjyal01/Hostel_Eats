1a4ba95fc6abbeea5705868b2130b7c1de9d41d8
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2024-06-12 18:49:11 +0530

    Update Password backend done

--- server/routes/register.js
+++ server/routes/register.js
@@ -4,7 +4,7 @@ const { userSignup } = require("../controllers/signup"); //For both customer and
 const {userLogin}=require("../controllers/login");
 const {auth,isCustomer,isOwner}=require("../middlewares/auth");
 const {addCanteen,addItem,getAllCanteen, getCanteenDetails,addItems}=require("../controllers/canteen");
-const {updateDisplayPicture, updateProfile,updateEmail}=require("../controllers/profileSettings");
+const {updateDisplayPicture, updateProfile,updateEmail,updatePassword}=require("../controllers/profileSettings");
 const {editCanteen,editItem,deleteItem,deleteCanteen}=require("../controllers/modifyCanteen")
 
 const {searchItem}=require("../controllers/customer");
@@ -38,7 +38,7 @@ router.post("/customer/searchItem",searchItem);
 router.post("/profile/updateDisplayPicture",updateDisplayPicture);
 router.post("/profile/updateProfile",updateProfile);
 router.post("/profile/updateEmail",updateEmail)
-
+router.post("/profile/updatePassword",updatePassword)
 
 
 //Protected Route
