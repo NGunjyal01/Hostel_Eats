e743a193bd4daefe99f45a27d8c72b406df4f768
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2024-06-18 01:50:56 +0530

    Remove Cart API Done

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const customerRouter = express.Router();
-const { searchItem, addItemToCart, getItemFromCart } = require("../controllers/customer");
+const { searchItem, addItemToCart, getItemFromCart, removeItemFromCart } = require("../controllers/customer");
 const { getCanteenDetails } = require("../controllers/customer");
 const {customerCheck}=require("../middlewares/customer");
 //Customer Routes
 //Routes for Search Item
 customerRouter.post("/searchItem",customerCheck,searchItem);
 customerRouter.get("/getCanteenDetails",customerCheck,getCanteenDetails);
-customerRouter.post("/setCartItems",customerCheck,addItemToCart);
+customerRouter.post("/addCartItem",customerCheck,addItemToCart);
 customerRouter.get("/getCartItems",customerCheck,getItemFromCart);
+customerRouter.post("/removeCartItem",customerCheck,removeItemFromCart);
 
 module.exports=customerRouter;
