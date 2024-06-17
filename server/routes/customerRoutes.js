9af0d3815dc12d7ba98c9bf60d831aa8f9fc92d3
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2024-06-17 18:53:18 +0530

    Add Cart and Get Cart items Backend Done

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const customerRouter = express.Router();
-const { searchItem } = require("../controllers/customer");
+const { searchItem, addItemToCart, getItemFromCart } = require("../controllers/customer");
 const { getCanteenDetails } = require("../controllers/customer");
-
+const {customerCheck}=require("../middlewares/customer");
 //Customer Routes
 //Routes for Search Item
-customerRouter.post("/searchItem",searchItem);
-customerRouter.get("/getCanteenDetails",getCanteenDetails);
+customerRouter.post("/searchItem",customerCheck,searchItem);
+customerRouter.get("/getCanteenDetails",customerCheck,getCanteenDetails);
+customerRouter.post("/setCartItems",customerCheck,addItemToCart);
+customerRouter.get("/getCartItems",customerCheck,getItemFromCart);
 
 module.exports=customerRouter;
