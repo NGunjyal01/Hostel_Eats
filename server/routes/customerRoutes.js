f082995a83b075db9c136a629e28ab24d426ce20
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2024-08-05 10:15:27 +0530

    update1

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -5,8 +5,8 @@ const { getCanteenDetails } = require("../controllers/customer");
 const {customerCheck}=require("../middlewares/customer");
 //Customer Routes
 //Routes for Search Item
-customerRouter.post("/searchItem",customerCheck,searchItem);
-customerRouter.get("/getCanteenDetails",customerCheck,getCanteenDetails);
+customerRouter.post("/searchItem",searchItem);
+customerRouter.get("/getCanteenDetails",getCanteenDetails);
 customerRouter.post("/searchItemByCanteen",customerCheck,searchItemByCanteen);
 customerRouter.post("/addCartItem",customerCheck,addItemToCart);
 customerRouter.get("/getCartItems",customerCheck,getItemFromCart);
